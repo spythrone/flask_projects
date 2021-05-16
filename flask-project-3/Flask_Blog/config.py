6f8daccd821105b49b7f96f40e6bd504f1a9ea4c
spythrone <pragyat021@gmail.com>
2021-05-16 08:09:32 +0530

    [UPDATE] Flask App

--- flask-project-3/Flask_Blog/config.py
+++
@@ -1,4 +0,0 @@
-class BaseConfig(object):
-    DEBUG = True
-    SECRET_KEY = '35327e19d209614d7150e4c45f8ec1a2'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://testuser:testuser@localhost:5432/testdb'
